2e488b1a45717540c688bb6ee2ee68516ff5998b
Dominic <dom.with.a.inic+git@gmail.com>
2018-07-23 11:12:29 +1000

    Fix install script

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 sourceDir="$(pwd)"/FontAndColorThemes/
-targetDir=~/Library/Developer/Xcode/UserData/FontAndColorThemes
+targetDir=~/Library/Developer/Xcode/UserData
 
+rm -rf ~/Library/Developer/Xcode/UserData/FontAndColorThemes/
 ln -s $sourceDir $targetDir
